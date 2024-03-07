3056716e0d720107a0f0263223fdc653cc04b775
angel <angel@DESKTOP-S83E2U3>
2024-03-06 23:51:55 -0300

    Versao final

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,7 @@
 package view;
 
+import javax.swing.JOptionPane;
+
 import controller.RedesController;
 
 public class Main {
@@ -7,10 +9,22 @@ public class Main {
 	public static void main(String[] args) {
 
 		RedesController redeController = new RedesController();
+		String i = JOptionPane.showInputDialog(null, "1 - IP\n 2 - PING\n 3 - SAIR", "Atividade OS",
+				JOptionPane.PLAIN_MESSAGE);
 
-		redeController.IP();
-		redeController.PING();
-
+		switch (Integer.parseInt(i)) {
+		case 1: {
+			redeController.IP();
+			break;
+		}
+		case 2: {
+			redeController.PING();
+			break;
+		}
+		default:
+			JOptionPane.showMessageDialog(null, "Adeus");
+			break;
+		}
 	}
 
 }
